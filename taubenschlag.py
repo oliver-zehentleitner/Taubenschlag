ee9cf9f3a82187917f9cc01c8c30e348c8225048
Oliver Zehentleitner <oliver.zehentleitner@unicorn-data.com>
2019-07-23 11:39:18 +0200

    0.9.2

--- taubenschlag.py
+++ taubenschlag.py
@@ -66,7 +66,7 @@ logging.getLogger('taubenschlag').setLevel(logging.INFO)
 
 class Taubenschlag(object):
     def __init__(self):
-        self.app_version = "0.9.1"
+        self.app_version = "0.9.2"
         self.config = self._load_config()
         self.app_name = self.config['SYSTEM']['app_name']
         self.dm_sender_name = self.config['SYSTEM']['dm_sender_name']
